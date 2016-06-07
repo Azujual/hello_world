19847d2b5834c679829b034260eb84b96ae6dbf7
Azujual <Serghei@infineon.md>
2016-06-07 20:46:02 +0300

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,15 +1,21 @@
 class Point {
 	int x,y;
+	final int COLOR = 1;
+	
 	Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
+	
 	double distance (int x, int y){
 		int dx = this.x - x;
 		int dy = this.y - y;
 		return Math.sqrt(dx * dx + dy * dy);
 	}
+	
 	double distance (Point p) {
 		return distance(p.x, p.y);
 	}
+	
+	
 }
