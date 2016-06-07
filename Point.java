bffa2a62e69cb8dcf67d2f9a62ed785dcce6ac18
Azujual <Serghei@infineon.md>
2016-06-07 19:20:24 +0300

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,15 @@
+class Point {
+	int x,y;
+	Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	double distance (int x, int y){
+		int dx = this.x - x;
+		int dy = this.y - y;
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+	double distance (Point p) {
+		return distance(p.x, p.y);
+	}
+}
